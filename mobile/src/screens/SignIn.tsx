baa1fa4a434d2dbbf2fb8df0399a6b12b1ab7adf
unknown <joao.pimentel@engenharia.ufjf.br>
2022-11-02 23:51:48 -0300

    mobile layout

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,28 @@
-import { Text, Center } from 'native-base';
+import { Text, Center, Icon } from 'native-base';
+import {Fontisto} from "@expo/vector-icons";
+
+import {useAuth} from "../hooks/useAuth";
+import { Button } from '../components/Button';
+import Logo from "../assets/logo.svg";
 
 export function SignIn(){
+  
+  const {signIn, user} = useAuth();
+  
   return (
-    <Center flex={1} bgColor="gray.900">
-      <Text color="white">Hello</Text>
+    <Center flex={1} bgColor="gray.900" p={7}>
+      <Logo width={212} height={40}/>
+      <Button 
+        title="Entrar com Google"
+        type="SECONDARY"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+        onPress={signIn}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação de sua conta.
+      </Text>
     </Center>
   )
 }
